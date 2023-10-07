9200f86d2ef3f2a3dbcbdc5f92dc4792924684e1
Zack Buhman <zack@buhman.org>
2023-10-07 21:05:59 +0000

    start: map dtcm

--- start.s
+++ start.s
@@ -10,6 +10,11 @@
 	ldr r1, =#((7 << 10) | (10 << 5) | (19 << 0))
 	strh r1, [r0, #0x000]
 
+	// enable dtcm, mapped to 0x027e0000
+	ldr r0, =#(0x027E0000 | (0b01101 << 1) | 1)
+	mcr p15, 0, r0, c6, c0, 0
+
+	// move sp to the end of dtcm
 	ldr r0, =#(0x027E0000 + 0x400 - 4)
 	mov sp, r0
 
