a95f3fe9eb81e4d3dd4fd2af275dd125ca6d1194
Zack Buhman <zack@buhman.org>
2023-10-08 01:33:01 +0000

    start both LCDs

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #define DB_DISPCNT__DISPLAY__ON (0b1 << 16)
 
 #define VRAMCNT 0x240
+#define VRAM_HI_CNT 0x248
 
 #define BGxCNT__COLOR_MODE__16 (0b0 << 7)
 #define BGxCNT__SCREEN_SIZE__256x256 (0b00 << 14)
@@ -19,18 +20,20 @@
 #define BG0CNT 0x008
 #define BG1CNT 0x00a
 
+#define DB_BG1CNT 0x100a
+
 void main()
 {
+  const uint32_t character_size = 8 * 8 / 2; // in bytes (8x8 @ 4bpp)
+
   volatile uint32_t * io_reg32 = (volatile uint32_t *)0x04000000;
   volatile uint16_t * io_reg16 = (volatile uint16_t *)0x04000000;
-  volatile uint16_t * bg_pram_a = (volatile uint16_t *)0x05000000;
-  volatile uint16_t * bg_pram_b = (volatile uint16_t *)0x05000400;
 
+  volatile uint16_t * bg_pram_a = (volatile uint16_t *)0x05000000;
   volatile uint32_t * bg_vram_a = (volatile uint32_t *)0x06000000;
-  //volatile uint8_t * bg_vram_b = (volatile uint8_t *)0x06200000;
 
   bg_pram_a[0] = (31 << 10) | (0 << 5) | (31 << 0);
-  bg_pram_a[1] = (31 << 10) | (0 << 5) | (3 << 0);
+  bg_pram_a[1] = (13 << 10) | (20 << 5) | (3 << 0);
 
   io_reg32[DISPCNT / 4] = DISPCNT__DISPLAY__GRAPHICS
                         | DISPCNT__BG1
@@ -45,12 +48,12 @@ void main()
   // - OFS: 00
   // - MST: 01
   // - E  :  1
-  io_reg32[VRAMCNT / 4] = (1 << 7) | (0b01 << 0);
+  io_reg32[VRAMCNT / 4] =
+    (1 << 7) | (0b01 << 0); // VRAM-A mapped to 0x06000000 for Graphics Engine A
 
   // define character number 1 in character base block 0
-  uint32_t character_size = 8 * 8 / 2; // in bytes (8x8 @ 4bpp)
   volatile uint32_t * character = &bg_vram_a[character_size / 4];
-  for (int i = 0; i < (character_size / 4) * 2; i++) {
+  for (int i = 0; i < (character_size / 4); i++) {
     // set color index 1, 8 pixels at a time
     character[i] = (1 << 28) | (1 << 24) | (1 << 20) | (1 << 16)
                  | (1 << 12) | (1 << 8)  | (1 << 4)  | (1 << 0);
@@ -59,7 +62,46 @@ void main()
   // screen
   // show character number 1 at the top-left corner
   volatile uint16_t * screen = (volatile uint16_t *)&bg_vram_a[31 * 0x800 / 4];
-  screen[0] = 1;
+
+  screen[5 * 32 + 10] = 1;
+
+
+  // now start the second screen
+
+  volatile uint16_t * bg_pram_b = (volatile uint16_t *)0x05000400;
+  volatile uint32_t * bg_vram_b = (volatile uint32_t *)0x06200000;
+
+  bg_pram_b[0] = (31 << 10) | (14 << 5) | (14 << 0);
+  bg_pram_b[1] = (3 << 10)  | (31 << 5)  | (31 << 0);
+
+  io_reg32[DB_DISPCNT / 4] = DB_DISPCNT__DISPLAY__ON
+                           | DISPCNT__BG1
+                           | DISPCNT__BG_MODE_0;
+
+  io_reg16[DB_BG1CNT / 2] = BGxCNT__SCREEN_SIZE__256x256
+                          | BGxCNT__SCREEN_BASE_BLOCK(2)
+                          | BGxCNT__COLOR_MODE__16
+                          | BGxCNT__CHARACTER_BASE_BLOCK(0);
+
+  // VRAM-H:
+  // - MST: 01
+  // - E  :  1
+  io_reg16[VRAM_HI_CNT / 2] =
+    (1 << 7) | (0b01 << 0); // VRAM-H mapped to 0x06200000 for Graphics Engine B
+
+  // define character number 1 in character base block 0
+  volatile uint32_t * character_b = &bg_vram_b[character_size / 4];
+  for (int i = 0; i < (character_size / 4); i++) {
+    // set color index 1, 8 pixels at a time
+    character_b[i] = (1 << 28) | (1 << 24) | (1 << 20) | (1 << 16)
+                   | (1 << 12) | (1 << 8)  | (1 << 4)  | (1 << 0);
+  }
+
+  // screen
+  // show character number 1 at the top-left corner
+  volatile uint16_t * screen_b = (volatile uint16_t *)&bg_vram_b[2 * 0x800 / 4];
+
+  screen_b[15 * 32 + 4] = 1;
 
   while (1) {};
 }
